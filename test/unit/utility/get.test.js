9e8fed6fd8f40ebd0e4516b47c002fd7d43839e3
Nathan Houle <nathan@nathanhoule.com>
2014-08-20 13:43:30 -0700

    Add `get`

---
+++ test/unit/utility/get.test.js
@@ -0,0 +1,42 @@
+import { get } from '../../../dist/utility/get';
+
+describe('get', function() {
+  it('should be a function', function() {
+    expect(get).to.be.a('function');
+  });
+
+  it('should have an arity of 2', function() {
+    expect(get.length).to.equal(2);
+  });
+
+  it('should be curried', function() {
+    var fn = get('omg');
+
+    expect(fn).to.be.a('function');
+
+    fn()()()()();
+
+    expect(fn).to.be.a('function');
+
+    expect(fn({ omg: 1 })).to.be.a('number');
+  });
+
+  it('should return a value when one is defined at a given key', function() {
+    expect(get('omg', { omg: 'yes' })).to.equal('yes');
+  });
+
+  it('should fetch falsy values', function() {
+    expect(get('omg', { omg: false })).to.be.false;
+  });
+
+  it('should return undefined when no property found', function() {
+    expect(get('omg', {})).to.be.undefined;
+  });
+
+  it('should gracefully handle bad values', function() {
+    expect(() => get('lol', null)).to.not.throw();
+    expect(get('lol', null)).to.be.undefined;
+    expect(() => get('lol', undefined)).to.not.throw();
+    expect(get('lol', undefined)).to.be.undefined;
+  });
+});
