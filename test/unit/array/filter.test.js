7d7c744e68475b2bd0c6049cab0947b3326518dd
Nathan Houle <nathan@nathanhoule.com>
2014-08-20 12:44:07 -0700

    Add `filter`

---
+++ test/unit/array/filter.test.js
@@ -0,0 +1,59 @@
+import { NOT_FUNC_EXCEPTION } from '../../../dist/internal/exceptions';
+import { filter } from '../../../dist/array/filter';
+import { identity } from '../../../dist/utility/identity';
+import { truthy } from '../../../dist/utility/truthy';
+
+describe('filter', function() {
+  var observe;
+
+  beforeEach(function() {
+    observe = sinon.spy(identity);
+  });
+
+  it('should be a function', function() {
+    expect(filter).to.be.a('function');
+  });
+
+  it('should have an arity of 2', function() {
+    expect(filter.length).to.equal(2);
+  });
+
+  it('should be curried', function() {
+    var fn = filter(observe);
+
+    expect(observe).to.have.not.beenCalled;
+
+    fn()()()();
+
+    expect(observe).to.have.not.beenCalled;
+
+    fn([1]);
+
+    expect(observe).to.have.been.calledOnce;
+  });
+
+  it('should return an array', function() {
+    expect(filter(observe, [])).to.eql([]);
+  });
+
+  it('should return a list excluding elements for which the predicate function returns `false`', function() {
+    var elems = ['', NaN, 0];
+
+    expect(filter(truthy, elems)).to.eql([]);
+  });
+
+  it('should return a list including elements for which the predicate function returns `true`', function() {
+    var elems = [1, 'a', '', { a: 'a' }, NaN];
+    var trues = [1, 'a', { a: 'a' }, Object];
+
+    expect(filter(truthy, elems)).to.eql([1, 'a', { a: 'a' }]);
+    expect(filter(truthy, trues)).to.eql(trues);
+  });
+
+  xit('should work on non-array-like structures', function() {
+  });
+
+  it('should throw an error when the provided predicate function is not a function', function() {
+    expect(() => filter('fdsa', [])).to.throw(NOT_FUNC_EXCEPTION);
+  });
+});
