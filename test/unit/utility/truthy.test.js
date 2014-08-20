e28816892fd5e36897ba9d974587826d02acdaac
Nathan Houle <nathan@nathanhoule.com>
2014-08-20 09:38:15 -0700

    Add truthy

---
+++ test/unit/utility/truthy.test.js
@@ -0,0 +1,31 @@
+import { truthy } from '../../../dist/utility/truthy';
+
+describe('falsy', function() {
+  it('should be a function', function() {
+    expect(truthy).to.be.a('function');
+  });
+
+  it('should have an arity of 1', function() {
+    expect(truthy.length).to.equal(1);
+  });
+
+  it('should return `false` when passed no arguments', function() {
+    expect(truthy()).to.be.false;
+  });
+
+  it('should return `false` when its argument is a falsy value', function() {
+    expect(truthy(undefined)).to.be.false;
+    expect(truthy(null)).to.be.false;
+    expect(truthy(false)).to.be.false;
+    expect(truthy('')).to.be.false;
+    expect(truthy(0)).to.be.false;
+    expect(truthy(NaN)).to.be.false;
+  });
+
+  it('should return `true` when its argument is any other value', function() {
+    expect(truthy('fdsa')).to.be.true;
+    expect(truthy(1)).to.be.true;
+    expect(truthy(true)).to.be.true;
+    expect(truthy({})).to.be.true;
+  });
+});
