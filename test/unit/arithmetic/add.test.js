fccb4d14ab384d0bd6d33bbb59aa6fc8507bae16
Nathan Houle <nathan@nathanhoule.com>
2014-08-23 16:39:34 -0700

    Add `add` function

---
+++ test/unit/arithmetic/add.test.js
@@ -0,0 +1,23 @@
+import { add } from '../../../dist/arithmetic/add';
+
+describe('add', function() {
+  it('should be a function', function() {
+    expect(add).to.be.a('function');
+  });
+
+  it('should have an arity of 2', function() {
+    expect(add.length).to.equal(2);
+  });
+
+  it('should be curried', function() {
+    expect(add(1)).to.be.a('function');
+    expect(add(1)()()()).to.be.a('function');
+    expect(add(1)(2)).to.be.a('number');
+    expect(add(1)()()(2)).to.be.a('number');
+  });
+
+  it('should do what the native operator would do', function() {
+    expect(add(1, 2)).to.equal(3);
+    expect(add(-1, 2)).to.equal(1);
+  });
+});
