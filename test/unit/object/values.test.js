0134b7cb732b8c438a57594ba18bed07b7417870
Nathan Houle <nathan@nathanhoule.com>
2014-08-27 17:47:16 -0700

    Add `values`

---
+++ test/unit/object/values.test.js
@@ -0,0 +1,41 @@
+import { values } from '../../../dist/object/values';
+
+describe('values', function() {
+  it('should be a function', function() {
+    expect(values).to.be.a('function');
+  });
+
+  it('should have an arity of 1', function() {
+    expect(values.length).to.equal(1);
+  });
+
+  it('should retrieve a list of all values from an object', function() {
+    var object = { a: 'aaa', b: 'bbb', c: 'ccc' };
+    var expected = ['aaa', 'bbb', 'ccc'].sort();
+
+    expect(values(object).sort()).to.eql(expected);
+  });
+
+  it('should ignore values on the object\'s prototype', function() {
+    var parent = { ignore: true };
+    var child = Object.create(parent);
+    child.a = 'aaa';
+    child.b = 'bbb';
+    child.c = 'ccc';
+    var expected = ['aaa', 'bbb', 'ccc'].sort();
+
+    expect(values(child).sort()).to.eql(expected);
+  });
+
+  it('should work on arrays', function() {
+    var array = [1, 2, 3];
+
+    expect(values(array)).to.eql(array);
+  });
+
+  it('should work on string objects', function() {
+    var string = new String('test');
+
+    expect(values(string)).to.eql(['t', 'e', 's', 't']);
+  });
+});
