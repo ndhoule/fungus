a3ddf387d9603b7f6a40fb7f3b9b3f5669940556
Nathan Houle <nathan@nathanhoule.com>
2014-08-28 00:48:28 -0700

    Add `has`

---
+++ test/unit/object/has.test.js
@@ -0,0 +1,39 @@
+import { has } from '../../../dist/object/has';
+
+describe('has', function() {
+  it('should be a function', function() {
+    expect(has).to.be.a('function');
+  });
+
+  it('should have an arity of 2', function() {
+    expect(has.length).to.equal(2);
+  });
+
+  it('should be curried', function() {
+    expect(has('test')).to.be.a('function');
+    expect(has('test')()()()).to.be.a('function');
+    expect(has('test')({})).to.be.a('boolean');
+    expect(has('test')()()({})).to.be.a('boolean');
+  });
+
+  it('should return `true` when an object has a specified property', function() {
+    expect(has('test', { test: 1 })).to.be.true;
+  });
+
+  it('should return `false` when an object does not have a specified property', function() {
+    expect(has('test', {})).to.be.false;
+    expect(has('test', { testz: 1 })).to.be.false;
+  });
+
+  it('should ignore inherited properties', function() {
+    var parent = { a: 'a' };
+    var child = Object.create(parent);
+
+    expect(has('a', child)).to.be.false;
+  });
+
+  it('should return `false` when passed `null` and `undefined` as target object', function() {
+    expect(has('test', null)).to.be.false;
+    expect(has('test', undefined)).to.be.false;
+  });
+});
