a1e05c3b464307eb4e02a3873d3178d78bcecf79
Nathan Houle <nathan@nathanhoule.com>
2014-09-01 12:30:49 -0700

    Add Sauce testing harness

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -1,53 +1,51 @@
 module.exports = function(config) {
   'use strict';
 
+  var customLaunchers = require('./browser-targets');
+
+  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+    process.exit(1);
+  }
+
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../..',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'sinon-chai', 'chai-factories', 'chai'],
 
-    // list of files / patterns to load in the browser
     files: [
       'dist/browser.min.js',
       'test/factories/**/*.js',
       'test/unit/**/*.test.js'
     ],
 
-    // list of files to exclude
     exclude: [],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {},
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'saucelabs'],
 
-    // web server port
     port: 9876,
 
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values:
-    //   config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_ERROR,
 
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    captureTimeout: 120000,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    sauceLabs: {
+      testName: 'Fungus',
+      recordScreenshots: false,
+      tags: ['fungus']
+    },
+
+    browsers: Object.keys(customLaunchers),
+
+    customLaunchers: customLaunchers,
+
     singleRun: true
   });
 };
